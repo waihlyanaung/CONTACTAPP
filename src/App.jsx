e4a382d62b8330ef0264aa513a9379ef838165bd
NiNiShyuneLae <heathersan865@gmail.com>
2023-06-03 14:39:57 +0630

    side nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
 
 const App = () => {
   return (
-    <div className='font-nunito'>
-      App
-      <div className="px-6 py-1 hover:bg-primary hover:text-white transition-all duration-200 rounded-full border-2 border-primary w-96 text-center uppercase tracking-wider font-semibold  text-primary">Login</div>
+    <div>
+      {/* <div className="px-6 py-1 hover:bg-primary hover:text-white transition-all duration-200 rounded-full border-2 border-primary w-96 text-center uppercase tracking-wider font-semibold  text-primary">Login</div> */}
+      <Routes>
+        <Route path='/' element={<Dashboard/>}/>
+      </Routes>
       </div>
   )
 }
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <Sidebar/>
+
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Dashboard
